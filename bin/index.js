c0488275c1e418658153be29b2417bceec181b64
Matt Styles <matt@Matts-MBP.cable.virginm.net>
2023-01-04 21:05:39 +0000

    Update import

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("Hello world!");
+require('dotenv').config();
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
+const configuration = new Configuration({
+    organization: "org-SA8UCjBrbhtyUaMZaCmWJXIU",
+    apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+});
+
+if (response.data.choices[0].text) {
+    console.log(response.data.choices[0].text);
+}
